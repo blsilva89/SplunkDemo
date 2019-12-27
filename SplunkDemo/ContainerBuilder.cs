d2649dc0b07fd614e209196f1d46d866bc02c8c0
blsilva89 <blsilva89@gmail.com>
2019-12-27 00:44:48 -0300

    Add project files.

---
+++ SplunkDemo/ContainerBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+
+namespace SplunkDemo
+{
+    public static class ContainerBuilder
+    {
+        public static IServiceProvider Configurar()
+        {
+            var colecaoServico = new ServiceCollection();
+            var config = new ConfigurationBuilder().SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                                                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                                   .Build();
+
+            NLog.LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+            colecaoServico.AddLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(LogLevel.Trace);
+                logging.AddNLog();                
+            });
+
+            return colecaoServico.BuildServiceProvider();
+        }
+    }
+}
---
+++ SplunkDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SplunkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IServiceProvider provedorServico = ContainerBuilder.Configurar();
+
+            var logger = provedorServico.GetService<ILogger<Program>>();
+
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+            logger.LogInformation("Processed {@Position} in {Elapsed:000} ms.", position, elapsedMs);
+
+            //var data = DateTime.Now.Date.ToString("dd/MM/yyyy");
+
+            //logger.LogInformation("We are logging in splunk {data}!", data);
+            //logger.LogWarning("Logging a warning to test!");
+        }
+    }
+}
